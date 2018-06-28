5e309f46a79759e5769a2d8c2348dc5605b0041d
Daniel Kubiak <dan.kubiak@gmail.com>
2018-06-28 15:53:45 -0700

    fixed to api-routes, fn base64(): fixes to webcam.js for cross-browser streaming

--- public/js/webcam.js
+++ public/js/webcam.js
@@ -1,44 +1,71 @@
 
 var vid = document.querySelector("#videoElement");
+let localStream;
+//const mediaVid = new HTMLMediaElement();
+const mediaSource = new MediaSource();
+const mediaStream = new MediaStream();
 
-//const mediaSource = new MediaSource();
-//vid.src = mediaSource
+
+
+
+
 function startCam() {
-    if (navigator.webkitGetUserMedia!=null) {
-
-        var options = { 
-            video:true, 
-            audio:false 
-        };  
-        navigator.webkitGetUserMedia(options, 
-            function(stream) { 
-            vid.src = window.webkitURL.createObjectURL(stream);
-            //vid.src = window.URL.HTMLMediaElement.srcObject
-            //const mediaSource = new MediaSource();
-            //vid.src = stream
-            //vid.src = mediaSource;
-            
-            localstream = stream;
-            vid.play();
-            console.log("streaming");
-        }, 
-            function(e) { 
-                console.log("background error : " + e);
-            }); 
-    } 
+    // Older browsers might not implement mediaDevices at all, so we set an empty object first
+if (navigator.mediaDevices === undefined) {
+    navigator.mediaDevices = {};
+  }
+  
+  // Some browsers partially implement mediaDevices. We can't just assign an object
+  // with getUserMedia as it would overwrite existing properties.
+  // Here, we will just add the getUserMedia property if it's missing.
+  if (navigator.mediaDevices.getUserMedia === undefined) {
+    navigator.mediaDevices.getUserMedia = function(constraints) {
+  
+      // First get ahold of the legacy getUserMedia, if present
+      var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+  
+      // Some browsers just don't implement it - return a rejected promise with an error
+      // to keep a consistent interface
+      if (!getUserMedia) {
+        return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+      }
+  
+      // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+      return new Promise(function(resolve, reject) {
+        getUserMedia.call(navigator, constraints, resolve, reject);
+      });
+    }
+  }
+  
+  navigator.mediaDevices.getUserMedia({ audio: false, video: true })
+  .then(function(stream) {
+    var video = document.querySelector('video');
+    // Older browsers may not have srcObject
+    if ("srcObject" in video) {
+      video.srcObject = stream;
+    } else {
+      // Avoid using this in new browsers, as it is going away.
+      video.src = window.URL.createObjectURL(stream);
+    }
+    video.onloadedmetadata = function(e) {
+      video.play();
+    };
+  })
+  .catch(function(err) {
+    console.log(err.name + ": " + err.message);
+  });
 }
 
 function closeCam() {
-    console.log('close');
-    vid.pause();
-    /*let stream = vid.srcObject;
+    console.log('close');  
+    let stream = vid.srcObject;
     let tracks = stream.getTracks();
 
     tracks.forEach(function(track) {
         track.stop();
     });
 
-  videoElem.srcObject = null;*/
+  vid.srcObject = null;
 
 }
  //---------------------
@@ -58,38 +85,21 @@ function snapshot() {
     ctx.drawImage(vid, 0,0, canvas.width, canvas.height);   
     canvas = document.getElementById("myCanvas");
     ctx = canvas.getContext('2d');        
-    dataURL = canvas.toDataURL('image/png');      
-    //console.log("URL: "+dataURL);
+    dataURL = canvas.toDataURL('image/png');    
     //console.log(dataURL);
     const dataSent = {
         key: dataURL
     }
     $.post("/api/logo", dataSent,
         function(data, status) {
-            //apiResponse();
-        })//.then(
-           setTimeout(apiResponse, 5000);
-        //);
-                
-        //apiResponse();
-    
-    
-    
-            //.then(response => response.json()) 
-            /*console.log("image sent");
-            resultDiv = document.getElementById("api-result");
-            console.log(data.json)
-            return data.json
-            resultDiv.html(data.body);*/
-            //.catch(error => console.error('Error:', error))
-            //.then(response => console.log('Success:', response));        
+            
+        })
+           setTimeout(apiResponse, 5000);             
 }
       
 function apiResponse() {
     $.get("/api/logo", function(data) {
-    console.log("res");
-    //resultDiv = document.getElementById("api-result");
-    //resultDiv.html(data);
+    console.log("res");    
     console.log(data)
     $('#api-result').text(data);
     
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,11 @@
-
-let myResult;
+let myRequest;
+let myResult = [];
 const vision = require('@google-cloud/vision');
 const client = new vision.ImageAnnotatorClient({ keyFilename: "./apikey.json" });
-function myVision(thisFile) {   
-    let fileName = thisFile;
+function myVision(fileName) {
+    //console.log("vision")
+    //let fileName = await base64()                   //thisFile;
+    //console.log("Filename: "+fileName)
     client
         .webDetection(fileName)
         .then(results => {
@@ -29,75 +31,10 @@ function myVision(thisFile) {
 }
 
 module.exports = (app) => {    
-    app.post("/api/logo", (req, res) => {      
-            try
-    {
-        // Decoding base-64 image
-        // Source: http://stackoverflow.com/questions/20267939/nodejs-write-base64-image-file
-        function decodeBase64Image(dataString) 
-        {
-          var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-          var response = {};
-
-          if (matches.length !== 3) 
-          {
-            return new Error('Invalid input string');
-          }
-
-          response.type = matches[1];
-          response.data = new Buffer(matches[2], 'base64');
-
-          return response;
-        }
-
-        // Regular expression for image type:
-        // This regular image extracts the "jpeg" from "image/jpeg"
-        const imageTypeRegularExpression      = /\/(.*?)$/;      
-
-        // Generate random string
-        const crypto                          = require('crypto');
-        const seed                            = crypto.randomBytes(20);
-        const uniqueSHA1String                = crypto
-                                               .createHash('sha1')
-                                                 .update(seed)
-                                                 .digest('hex');
-        
-        const base64Data =   req.body.key;       
-
-        const imageBuffer                      = decodeBase64Image(base64Data);
-        const userUploadedFeedMessagesLocation = "./"    
-
-        const uniqueRandomImageName            = 'image-' + uniqueSHA1String;
-        // This variable is actually an array which has 5 values,
-        // The [1] value is the real image extension
-        const imageTypeDetected                = imageBuffer
-                                                .type
-                                                 .match(imageTypeRegularExpression);
-
-        const userUploadedImagePath            = userUploadedFeedMessagesLocation + 
-                                               uniqueRandomImageName +
-                                               '.' + 
-                                               imageTypeDetected[1];
-
-        // Save decoded binary image to disk
-        try
-        {
-            require('fs').writeFile(userUploadedImagePath, imageBuffer.data,  
-                function() 
-                {
-                    console.log('DEBUG - feed:message: Saved to disk image attached by user:', userUploadedImagePath);                                
-                    myVision(uniqueRandomImageName+'.png');                                  
-                });                     
-        }
-        catch(error)
-        {
-            console.log('ERROR:', error);
-        }
-        }
-        catch(error)
-        {
-            console.log('ERROR:', error);
-        }    
+    app.post("/api/logo", (req, res) => {    
+        myRequest = req.body.key
+        console.log(myRequest);        
+        base64();             
     });
 
     app.get("/api/logo", function(req, res) {             
@@ -105,3 +42,84 @@ module.exports = (app) => {
         return res.json(myResult);
     }); 
 }
+async function base64() {
+try
+{
+    // Decoding base-64 image
+    // Source: http://stackoverflow.com/questions/20267939/nodejs-write-base64-image-file
+    function decodeBase64Image(dataString) 
+    {
+      const matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+      let response = {};
+
+      if (matches.length !== 3) 
+      {
+        return new Error('Invalid input string');
+      }
+
+      response.type = matches[1];
+      response.data = new Buffer(matches[2], 'base64');
+
+      return response;
+    }
+
+    // Regular expression for image type:
+    // This regular image extracts the "jpeg" from "image/jpeg"
+    const imageTypeRegularExpression      = /\/(.*?)$/;      
+
+    // Generate random string
+    const crypto                          = require('crypto');
+    const seed                            = crypto.randomBytes(20);
+    const uniqueSHA1String                = crypto
+                                           .createHash('sha1')
+                                             .update(seed)
+                                             .digest('hex');
+    
+   // const base64Data =   req.body.key; 
+   const base64Data = myRequest;        
+
+    const imageBuffer                      = decodeBase64Image(base64Data);
+    const userUploadedFeedMessagesLocation = "./"    
+
+    const uniqueRandomImageName            = 'image-' + uniqueSHA1String;
+    // This variable is actually an array which has 5 values,
+    // The [1] value is the real image extension
+    const imageTypeDetected                = imageBuffer
+                                            .type
+                                             .match(imageTypeRegularExpression);
+
+    const userUploadedImagePath            = userUploadedFeedMessagesLocation + 
+                                           uniqueRandomImageName +
+                                           '.' + 
+                                           imageTypeDetected[1];
+
+    // Save decoded binary image to disk
+    try
+    {
+        const prom = new Promise(function(resolve, reject) {
+
+        
+        require('fs').writeFile(userUploadedImagePath, imageBuffer.data,  
+            function() 
+            {
+                console.log('DEBUG - feed:message: Saved to disk image attached by user:', userUploadedImagePath);                                
+               // myVision(uniqueRandomImageName+'.png');    
+                //return uniqueRandomImageName+'.png' 
+                resolve(uniqueRandomImageName+'.png')                             
+            }); 
+        });
+        prom.then(function(value) {
+            myVision(value)
+        })                        
+    }
+    catch(error)
+    {
+        console.log('ERROR:', error);
+    }
+    }
+    catch(error)
+    {
+        console.log('ERROR:', error);
+    }  
+}
+
